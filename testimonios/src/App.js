88ba25459fc203dd505c152fd1e7bd7866868445
Jeff Ramos <jefryr90@gmail.com>
2025-04-02 16:55:12 -0500

    Primer proyecto v2. terminado

--- testimonios/src/App.js
+++ testimonios/src/App.js
@@ -5,8 +5,31 @@ function App() {
   return (
     <div className='App'>
       <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros alumnos: </h1>
-        <Testimonio/>
+        <h1>Estos son nuestros docentes </h1>
+        <Testimonio
+          nombre='Alexis'
+          pais='Irlanda'
+          imagen='alexis'
+          cargo='Oftalmólogo'
+          empresa='Oftalmoderm'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Oftalmoderm'
+        />
+        <Testimonio
+          nombre='Alfredo'
+          pais='Holanda'
+          imagen='alfredo'
+          cargo='Neurocirujano'
+          empresa='Sanna'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Oftalmoderm'
+        />
+        <Testimonio
+          nombre='Cesar'
+          pais='Perú'
+          imagen='cesar'
+          cargo='Cirugía de tórax y cardiovascular'
+          empresa='UPCH'
+          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Oftalmoderm'
+        />
       </div>
     </div>
   );
--- testimonios/src/componentes/testimonio.js
+++ testimonios/src/componentes/testimonio.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio () {
+function Testimonio (props) {
   return (
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio' 
-        src={require('../imágenes/testimonio-alejandra.jpeg')} 
-        alt='Foto de Alejandra'
+        src={require(`../imágenes/testimonio-${props.imagen}.jpg`)} 
+        alt='Foto del profesional'
         height='350px'
       />    
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Alejandra en Irlanda</p>
-        <p className='cargo-testimonio'>Ingeniera de Software</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify "</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
