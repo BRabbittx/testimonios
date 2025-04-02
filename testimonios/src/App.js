fec1200a02956b56c003e93a2b361afc0c0ccb5a
Jeff <jefryr90@gmail.com>
2025-04-01 20:18:24 -0500

    Actualización 1 casa

--- testimonios/src/App.js
+++ testimonios/src/App.js
@@ -3,8 +3,11 @@ import Testimonio from './componentes/testimonio.js'
 
 function App() {
   return (
-    <div className="App">
-      <Testimonio/>
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos: </h1>
+        <Testimonio/>
+      </div>
     </div>
   );
 }
--- testimonios/src/componentes/testimonio.js
+++ testimonios/src/componentes/testimonio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
 
 function Testimonio () {
   return (
@@ -7,12 +8,12 @@ function Testimonio () {
         className='imagen-testimonio' 
         src={require('../imágenes/testimonio-alejandra.jpeg')} 
         alt='Foto de Alejandra'
-        height='200px'
+        height='350px'
       />    
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>Alejandra en Irlanda</p>
         <p className='cargo-testimonio'>Ingeniera de Software</p>
-        <p className='texto-testimonio'>Lore ipsum lore data anar blod</p>
+        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify "</p>
       </div>
     </div>
   );
